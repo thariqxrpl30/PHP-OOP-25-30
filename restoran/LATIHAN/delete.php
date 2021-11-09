ae4820935e5dcb881ce6ca7c87983a7b2bc63de1
ThariqXIRPL29 <87298575+thariqxrpl30@users.noreply.github.com>
2021-11-09 15:12:03 +0700

    Add files via upload

---
+++ restoran/LATIHAN/delete.php
@@ -0,0 +1,13 @@
+<?php 
+
+    require_once "../function.php";
+
+    
+
+    $sql = "DELETE FROM tblkategori WHERE idkategori = $id";
+
+    $result = mysqli_query($koneksi,$sql);
+
+    header("location:http://localhost/phpsmk/restoran/kategori/select.php");
+
+?>
---
+++ restoran/user/select.php
@@ -0,0 +1,1 @@
+<h1> Select user</h1>
